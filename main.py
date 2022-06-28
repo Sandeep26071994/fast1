0e544fc645fcd4486a826ef8b747ec44e51e8c7f
Sandeep26071994 <103501540+Sandeep26071994@users.noreply.github.com>
2022-06-29 00:03:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+from typing import Optional
 from fastapi import FastAPI
+from typing import Optional
 
+from pydantic import BaseModel
 app= FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Vinay'}}
+@app.get('/blog')
+def index(limit=10, published:bool=True, sort:Optional[str]=None):
+    return published
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from te db list'}
 
-@app.get('/about')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data':id}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit=10):
+    return {'data':{'1','2'}}
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published: Optional[bool]
+    
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':"Blog is created with title as {request.title}"}
