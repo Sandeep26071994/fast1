0b8c645e16c26ced41bae53f0f620378a7797845
Sandeep26071994 <103501540+Sandeep26071994@users.noreply.github.com>
2022-06-29 00:00:32 +0530

    Create main.py

---
+++ blog/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from blog.routers import authentication
+from . import models
+from .database import engine
+from sqlalchemy.orm import Session
+from .routers import blog,user
+
+app= FastAPI()
+
+
+models.Base.metadata.create_all(engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
